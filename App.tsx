696e38b88feda2fc1c9f483708d413d6d34013f3
Roman Pavlov <romlho13@gmail.com>
2024-11-14 20:57:02 +0400

    Added redux persist

--- App.tsx
+++ App.tsx
@@ -3,49 +3,53 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './src/redux/store';
+
 import { HomeScreen } from './src/screens/HomeScreen';
 import { DriverDetailsScreen } from './src/screens/DriverDetailsScreen';
 import { Colors } from './src/constants/colors';
-import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: Colors.warmRed,
-            },
-            headerTintColor: Colors.white,
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontFamily: 'Formula1',
-            },
-          }}
-        >
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ title: 'F1 Drivers' }}
-          />
-          <Stack.Screen
-            name="DriverDetails"
-            component={DriverDetailsScreen}
-            options={({ route }) => ({
-              title: `${route.params.driver.givenName} ${route.params.driver.familyName}`,
-              headerBackTitleStyle: {
-                fontFamily: 'Formula1',
-                fontSize: 13,
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: Colors.warmRed,
               },
-            })}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+              headerTintColor: Colors.white,
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontFamily: 'Formula1',
+              },
+            }}
+          >
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ title: 'F1 Drivers' }}
+            />
+            <Stack.Screen
+              name="DriverDetails"
+              component={DriverDetailsScreen}
+              options={({ route }) => ({
+                title: `${route.params.driver.givenName} ${route.params.driver.familyName}`,
+                headerBackTitleStyle: {
+                  fontFamily: 'Formula1',
+                  fontSize: 13,
+                },
+              })}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,44 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE, Storage } from 'redux-persist';
+import { MMKV } from 'react-native-mmkv';
+
 import { driversPageSlice } from './driversPageSlice';
 import { racesPageSlice } from './racesPageSlice';
 
+const MMKVstorage = new MMKV();
+const storage: Storage = {
+  setItem: async (name, value) => {
+    return MMKVstorage.set(name, JSON.stringify(value));
+  },
+  getItem: async (name) => {
+    const value = MMKVstorage.getString(name);
+    return (value && JSON.parse(value)) ?? null;
+  },
+  removeItem: async (name) => {
+    return MMKVstorage.delete(name);
+  },
+};
+
+const driversPageReducerPersistConfig = {
+  key: 'driversPageReducer',
+  storage,
+};
+
 export const store = configureStore({
   reducer: combineReducers({
-    driversPage: driversPageSlice.reducer,
+    driversPage: persistReducer(driversPageReducerPersistConfig, driversPageSlice.reducer),
     racesPage: racesPageSlice.reducer,
   }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppStore = typeof store;
 export type RootState = ReturnType<typeof store.getState>;
